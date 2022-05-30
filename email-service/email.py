73ecc7e687a49936d22647a69ad8339fb50f77d6
Xavier Collantes <xcollantes@google.com>
2022-05-30 23:51:00 +0000

    email

---
+++ email-service/email.py
@@ -0,0 +1,22 @@
+import logging
+from flask import Flask, request
+
+logging.basicConfig(level=logging.DEBUG)
+
+app = Flask(__name__)
+
+
+@app.route("/", methods=["POST"])
+def intake():
+    """Listen for PubSub reports."""
+    try:
+        report = request.data
+        j = request.get_json()
+        logging.info("Data from PubSub: %s", j)
+    except Exception as e:
+        logging.error(e)
+        return "ERROR"
+
+
+if __name__ == "__main__":
+    app.run(port=8080, host="0.0.0.0", debug=True)
